2d247f4c756d692ac13e8b4f30c3d31d254661d9
snehal <snehalsp65@gmail.com>
2023-11-06 23:36:48 +0530

    Mainproject UI done

--- src/Main project -1/Mainproject1.jsx
+++ src/Main project -1/Mainproject1.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import './Mainproject.css'
+import photo from './pc.avif'
 
 const Mainproject1 = () => {
+  const [result, setResult] = useState(''); 
+
   return (
     <div>
-      hello
+      <div className="headdiv">
+        <div className="partone">
+          
+        <img src={photo} alt="" />
+        </div>
+
+        <div className="parttwo">
+          <div className="titlediv">
+            <div className="wfh">Work From Home</div>
+            <div className="task">Task Productivity</div>
+            <div className="ent">Enhancer</div>
+            
+          </div>
+
+          <div className="taskdiv">
+            <div className="taskfill">   <input type="text" className="textinput" placeholder="Enter your task here" /></div>
+            <div className="predictor">Predict Time Using AI</div>
+          </div>
+
+          <div className="result">
+            <div className="resultin">{result}</div>
+          
+          </div>
+
+
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Mainproject1
-
+export default Mainproject1;
